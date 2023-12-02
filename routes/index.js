7b57a980107cc231e44da61153a43cdb6b583321
ShwetaD01 <shwetadonode819@gmail.com>
2023-12-02 12:42:35 +0530

    add: uploader

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,75 @@ var express = require('express');
 var router = express.Router();
 const userModel = require('./users');
 const postModel = require("./post");
+const passport = require('passport');
+const upload = require("./multer")
+const localStrategy = require("passport-local")
+passport.use(new localStrategy(userModel.authenticate()))
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/profile', isLoggedIn, async function(req, res, next) {
+
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  })
+  res.render('profile', {user});
+});
+router.get('/feed', function(req, res, next) {
+  res.render("feed");
+});
+router.post('/upload',upload.single("file"), function(req, res, next) {
+  if(!req.file){
+    return res.status(404).send("No files were given")
+  }
+  res.send("File uploaded successfully!!!!")
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {error: req.flash('error')});
+});
+
+router.post("/register", function(req, res){
+  let {username, email,fullName } = req.body
+  let userData = new userModel({username, email, fullName })
+  userModel.register(userData,req.body.password)
+  .then(function(){
+    passport.authenticate('local')(req, res, function(){
+      res.redirect("/")
+    })
+    
+  })
 
 
-router.get('/create', async function(req, res){
- let createdUser = await userModel.create({
-    username: "nikita",
-    password: "nikita@27",
-    posts: [],
-    email: "nikita@gmail.com",
-    fullName: "Nikita Kishor Donode"
-  });
-  res.send(createdUser)
 })
+router.post("/login",passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/login",
+  failureFlash: true
+}), function(req, res){
+  let {username, email,fullName } = req.body
+  let userData = new userModel({username, email, fullName })
+  userModel.register(userData,req.body.password)
+  .then(function(){
+    passport.authenticate('local')(req, res, function(){
+      res.redirect("/")
+    })
+    
+  })
+
+
+})
+
+router.get('/logout', function(req, res, next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+})
+
+function isLoggedIn(req,res, next){
+  if(req.isAuthenticated()) return next();
+  res.redirect("/");
+}
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-
+const plm = require("passport-local-mongoose")
 //  mongoose.connect();
 
  mongoose.connect(
     "mongodb://127.0.0.1:27017/pit",
-    // options
   )
   .then(()=>console.log('connected'))
   .catch(e=>console.log(e));
@@ -17,14 +16,11 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
     },
     posts: [],
     dp: {
         type: String,
-      
     },
-    
     email: {
         type: String,
         required: true,
@@ -35,7 +31,7 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
-
+userSchema.plugin(plm);
 // Create the User model
 const User  = mongoose.model('User', userSchema);
 
